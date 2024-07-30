ff6a3ddb8f0c66a8a2042ff2fe4ed071b3f0c929
Kat Marchán <kzm@zkat.tech>
2024-07-29 19:43:09 -0700

    sketch out character sheet idea

--- lib/black_iron_web/controllers/character_sheet_html.ex
+++ lib/black_iron_web/controllers/character_sheet_html.ex
@@ -5,7 +5,83 @@ defmodule BlackIronWeb.CharacterSheetHTML do
 
   def show(assigns) do
     ~H"""
-    This is the character sheet!
+    <bi-character-sheet>
+      <%# Rendered when there's no active character. %>
+      <article slot="placeholder">
+        <header><%= gettext("Character Sheet") %></header>
+        <p><%= gettext("Select a character to view their sheet.") %></p>
+        <bi-character-picker />
+      </article>
+      <%# Rendered when we have an active character in the context. %>
+      <article slot="sheet">
+        <header><%= gettext("Character Sheet") %></header>
+        <.character_info />
+        <.character_stats />
+        <.character_meters />
+        <.character_special_tracks />
+        <.character_impacts />
+        <.character_assets />
+      </article>
+    </bi-character-sheet>
+    """
+  end
+
+  defp character_info(assigns) do
+    ~H"""
+    <section class="info">
+      <header>
+        <bi-character-portrait />
+        <bi-character-name />
+      </header>
+      <bi-character-initiative />
+      <dl>
+        <dt><bi-character-alias-label /></dt>
+        <dd><bi-character-alias /></dd>
+        <dt><%= gettext("Pronouns") %></dt>
+        <dd><bi-character-pronouns /></dd>
+        <dt><%= gettext("Description") %></dt>
+        <dd><bi-character-description /></dd>
+        <dt><%= gettext("Player") %></dt>
+        <dd><bi-character-player /></dd>
+        <dt><%= gettext("Experience") %></dt>
+        <dd><bi-character-xp /></dd>
+      </dl>
+    </section>
+    """
+  end
+
+  defp character_stats(assigns) do
+    ~H"""
+    <section class="stats">
+    </section>
+    """
+  end
+
+  defp character_meters(assigns) do
+    ~H"""
+    <section class="meters">
+    </section>
+    """
+  end
+
+  defp character_special_tracks(assigns) do
+    ~H"""
+    <section class="special-tracks">
+    </section>
+    """
+  end
+
+  defp character_impacts(assigns) do
+    ~H"""
+    <section class="impacts">
+    </section>
+    """
+  end
+
+  defp character_assets(assigns) do
+    ~H"""
+    <section class="assets">
+    </section>
     """
   end
 end
