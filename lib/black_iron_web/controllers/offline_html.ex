e61fd6e09de69538625c07958d9ed33bda6bce17
Kat Marchán <kzm@zkat.tech>
2024-07-30 09:12:41 -0700

    more locale stuff for site, generate .po files, Spanish translation

--- lib/black_iron_web/controllers/offline_html.ex
+++ lib/black_iron_web/controllers/offline_html.ex
@@ -5,6 +5,8 @@ defmodule BlackIronWeb.OfflineHTML do
   use BlackIronWeb, :html
 
   def site(assigns) do
+    # TODO(@zkat): gettext text here when we have an actual offline page
+    # designed.
     ~H"""
     You're offline. Refresh when you have internet again.
     """
