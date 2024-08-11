b38ee6dd18dc52b879fe28d362e4c7e24ca06832
Kat Marchán <kzm@zkat.tech>
2024-08-11 00:09:02 -0700

    register service worker on dom load

--- assets/js/install-sw.ts
+++ assets/js/install-sw.ts
@@ -1,17 +1,26 @@
 export async function installServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("/service-worker.js");
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-      await navigator.serviceWorker.ready;
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
+  return new Promise((resolve, reject) => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", async () => {
+        try {
+          const registration =
+            await navigator.serviceWorker.register("/service-worker.js");
+          if (registration.installing) {
+            console.log("Service worker installing");
+          } else if (registration.waiting) {
+            console.log("Service worker installed");
+          } else if (registration.active) {
+            console.log("Service worker active");
+          }
+          await navigator.serviceWorker.ready;
+          resolve(undefined);
+        } catch (error) {
+          console.error(`Registration failed with ${error}`);
+          reject(error);
+        }
+      });
+    } else {
+      resolve(undefined);
     }
-  }
+  });
 }
