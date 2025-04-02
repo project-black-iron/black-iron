56d3a299da577079a30cf66f2bdde7ca4e10bdb2
Kat Marchán <kzm@zkat.tech>
2025-04-02 15:00:32 -0700

    set up assets before ecto so LitSSR can run

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule BlackIron.MixProject do
 
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "assets.setup", "assets.build", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate --quiet", "test"],
